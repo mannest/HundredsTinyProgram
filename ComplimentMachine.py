5b998411df3f3c1d89ae3baf0601f092937dd526
Manne <manne.stenstrom@gmail.com>
2026-03-25 22:53:12 +0100

    Update 1

--- ComplimentMachine.py
+++ ComplimentMachine.py
@@ -1,3 +1,6 @@
+""" This is a Compliment Machine """
+import random
+import time
 
 list = ["You have a very thoughtful way of looking at things.",
         "Your ideas are always interesting and insightful.",
@@ -5,6 +8,10 @@ list = ["You have a very thoughtful way of looking at things.",
         "You have a great sense of curiosity and willingness to learn.",
         "Your work shows real dedication and effort."    ]
 
-for i in list:
-    print(i)
-    
+while True:
+        new_list = list
+        while len(new_list) >= 1:
+                i = random.randint(0, (len(new_list)-1))
+                print(new_list[i])
+                del new_list[i]
+                time.sleep(1)
