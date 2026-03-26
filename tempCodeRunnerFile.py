85140cb88b486d7fd6b7d415ada624d61daebdbf
Manne <manne.stenstrom@gmail.com>
2026-03-26 18:39:56 +0100

    shell version 0.1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,1 @@
-""" This is a Compliment Machine """
-import random
-import time
-
-list = ["You have a very thoughtful way of looking at things.",
-        "Your ideas are always interesting and insightful.",
-        "You explain complex things clearly and effectively.",
-        "You have a great sense of curiosity and willingness to learn.",
-        "Your work shows real dedication and effort."    ]
-
-while True:
-        new_list = list
-        while len(new_list) >= 1:
-                i = random.randint(0, (len(new_list)-1))
-                print(new_list[i])
-                del new_list[i]
-                time.sleep(5)
+ 
